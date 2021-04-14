c10146fa1fe1fd6e52ca8fc46cfc07b72f2dc098
noadonna <noadonna@users.noreply.github.com>
2021-04-14 22:33:59 +0200

    Added Axios and integrated Dictionary API

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import logo from "./dictionary-logo.png";
 import './App.css';
+import Dictionary from "./Dictionary"
 
 function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
         <img src={logo}
         className="App-logo img-fluid" alt="logo" />
       </header>
+      <main>
+        <Dictionary />
+      </main>
+      <footer className="App-footer">
+        <a href="https://github.com/noadonna/shecodes-dictionary" target="_blank" rel="noreferrer">Open-source code</a>
+        {" "}by{" "} <a href="https://www.linkedin.com/in/carinevaneekelen" target="_blank" rel="noreferrer">Carine</a>
+      </footer>
+      </div>
     </div>
   );
 }
